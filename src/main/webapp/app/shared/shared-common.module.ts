0c74de8344e0cddc0ef648855ec5ee0fc626acfc
LiRichen <richard.li@esenyun.com>
2018-04-01 17:20:59 +0800

    Initial application generated by JHipster-4.14.0

---
+++ src/main/webapp/app/shared/shared-common.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { Jhipster5SharedLibsModule, FindLanguageFromKeyPipe, JhiAlertComponent, JhiAlertErrorComponent } from './';
+
+@NgModule({
+  imports: [Jhipster5SharedLibsModule],
+  declarations: [FindLanguageFromKeyPipe, JhiAlertComponent, JhiAlertErrorComponent],
+  providers: [],
+  exports: [Jhipster5SharedLibsModule, FindLanguageFromKeyPipe, JhiAlertComponent, JhiAlertErrorComponent]
+})
+export class Jhipster5SharedCommonModule {}
